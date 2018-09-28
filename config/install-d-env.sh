92650594527088c94edd55c38c1673c64d6e445e
ethanpreker <ethan.reker@wwinc.com>
2018-09-28 15:21:40 -0500

    initialize repo

---
+++ config/install-d-env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "*********************************************"
+echo "Installing D development environment"
+echo "*********************************************"
+brew install dub
+brew install ldc
+brew install dmd
+brew install python
+echo "*********************************************"
+echo "Finished installing D Development Environment"
+echo "*********************************************"
